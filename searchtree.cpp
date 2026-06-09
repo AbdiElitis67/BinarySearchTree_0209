725a0a713ce5132a2bb77db1309b33a5510e78e8
Abdielitis67 <meirdanz4@gmail.com>
2026-06-09 11:26:15 +0700

    root logic

--- searchtree.cpp
+++ searchtree.cpp
@@ -57,3 +57,37 @@ public:
             parent->rightchild = newnode;
         }
     }
+
+    // this function searches the current node of the specified node
+    // as well as the current node of its parent
+    void search(string element, node *&parent, node *&currentnode)
+    {
+        currentnode = root;
+        parent = NULL;
+
+        while (currentnode != NULL && currentnode->info != element)
+        {
+            parent = currentnode;
+
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
+        }
+    }
+
+    void inorder(node *ptr)
+    {
+        if (root == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if(ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
